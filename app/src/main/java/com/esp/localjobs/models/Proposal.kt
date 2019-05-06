0055796c834617857791cb8a75bf0a7e944b5467
Luca Moroldo <44212562+lucamoroz@users.noreply.github.com>
2019-05-07 01:01:16 +0200

    Added data objects: Job, Proposal, Location (#15)

---
+++ app/src/main/java/com/esp/localjobs/models/Proposal.kt
@@ -0,0 +1,12 @@
+package com.esp.localjobs.models
+
+data class Proposal(
+    var uid: String? = "",
+    var city: String? = "",
+    var range: Int? = 0,
+    var active: Boolean? = false,
+    var location: Location?,
+    var salary: Int? = 0,
+    var title: String? = "",
+    var desc: String? = ""
+)
