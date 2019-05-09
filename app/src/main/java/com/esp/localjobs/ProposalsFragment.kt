9498ad79db0508af488b9a6ef76f188020d5e34d
luca <lucamoro96@gmail.com>
2019-05-09 18:23:36 +0200

    search toolbar on proposals fragment

--- app/src/main/java/com/esp/localjobs/ProposalsFragment.kt
+++ app/src/main/java/com/esp/localjobs/ProposalsFragment.kt
@@ -3,8 +3,11 @@ package com.esp.localjobs
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_jobs.*
 
 /**
  * Fragment used to display a list of proposals
@@ -17,5 +20,25 @@ class ProposalsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_proposals, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // setup search toolbar
+        searchToolbar.inflateMenu(R.menu.menu_search)
+        searchToolbar.setOnMenuItemClickListener { menuItem -> handleSearchMenuItemClick(menuItem) }
+    }
+
+    /**
+     * Handle search toolbar item click.
+     * On filter item click: navigate to filter fragment
+     */
+    private fun handleSearchMenuItemClick(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.filter_search_item -> findNavController().navigate(R.id.action_destination_proposals_to_destination_filter)
+            else -> TODO()
+        }
+        return true
     }
 }
