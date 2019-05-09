2f02bc459e61ff16a65d7cffd6882b93b07582c3
luca <lucamoro96@gmail.com>
2019-05-09 18:18:35 +0200

    floating button

--- app/src/main/java/com/esp/localjobs/FilterResultsFragment.kt
+++ app/src/main/java/com/esp/localjobs/FilterResultsFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * Fragment used to set filter params (longitude, latitude, range, tags)
  */
 class FilterResultsFragment : Fragment() {
+    private val args: FilterResultsFragmentArgs by navArgs()
+    private var filteringJobs: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +22,18 @@ class FilterResultsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_filter_results, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        filteringJobs = args.filteringJobs
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            if (filteringJobs)
+                findNavController().navigate(R.id.action_destination_filter_to_destination_jobs)
+            else
+                findNavController().navigate(R.id.action_destination_filter_to_destination_proposals)
+        }
     }
 }
