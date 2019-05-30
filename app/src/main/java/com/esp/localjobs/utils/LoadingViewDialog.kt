414df0e98b4ae9f2a2c2d1c8a379f0d3d4039f9f
luca <lucamoro96@gmail.com>
2019-05-30 12:36:33 +0200

    Used kotlin syntax in loading dialog

--- app/src/main/java/com/esp/localjobs/utils/LoadingViewDialog.kt
+++ app/src/main/java/com/esp/localjobs/utils/LoadingViewDialog.kt
@@ -9,26 +9,23 @@ import com.bumptech.glide.request.target.GlideDrawableImageViewTarget
 import com.esp.localjobs.R
 
 class LoadingViewDialog(private val activity: Activity) {
-
     private lateinit var dialog: Dialog
 
     fun showDialog() {
-        dialog = Dialog(activity)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        // set cancelable false so that it's never get hidden
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.loading)
+        dialog = Dialog(activity).apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setCancelable(false)
+            setContentView(R.layout.loading)
+        }
+
         val gifImageView: ImageView = dialog.findViewById(R.id.loading_gif)
-
         val imageViewTarget = GlideDrawableImageViewTarget(gifImageView)
-
         Glide.with(activity)
             .load(R.drawable.loading)
             .placeholder(R.drawable.loading)
             .centerCrop()
             .crossFade()
             .into(imageViewTarget)
-
         dialog.show()
     }
 
