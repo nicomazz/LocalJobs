7a8ebcbe477c61c419bf5a22af75eb93ba6644df
nicomazz <nicomazz97@gmail.com>
2019-06-01 01:28:17 +0100

    trying to remove tests

--- app/src/test/java/com/esp/localjobs/data/MockRepository.kt
+++ app/src/test/java/com/esp/localjobs/data/MockRepository.kt
@@ -1,17 +1,10 @@
 package com.esp.localjobs.data
 
-import com.esp.localjobs.DI.RepositoryModule
-import com.esp.localjobs.data.base.BaseRepository
-import com.esp.localjobs.data.base.FirebaseDatabaseRepository
-import com.esp.localjobs.data.models.Job
-import com.esp.localjobs.viewModels.JobsViewModel
-import com.google.firebase.firestore.CollectionReference
-import dagger.Component
+/*
 
 class MockRepository : BaseRepository<Job> {
 
     companion object {
-        const val NUMBER_OF_JOBS = 3
     }
 
     private val fakeJobs = 0.until(NUMBER_OF_JOBS).map { Job() }
@@ -55,4 +48,5 @@ class MockRepositoryModule : RepositoryModule() {
 @Component(modules = [(MockRepositoryModule::class)])
 interface TestViewModelInjector {
     fun inject(presenter: JobsViewModel)
-}
+
+}*/
