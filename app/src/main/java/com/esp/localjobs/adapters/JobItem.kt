e4b754ebefb688c757da47ae863e5acd898646c2
nicomazz <nicomazz97@gmail.com>
2019-06-02 11:31:19 +0100

    Small fixes

--- app/src/main/java/com/esp/localjobs/adapters/JobItem.kt
+++ app/src/main/java/com/esp/localjobs/adapters/JobItem.kt
@@ -13,23 +13,26 @@ import com.xwray.groupie.databinding.BindableItem
 class JobItem(val job: Job, private val isSelected: Boolean) : BindableItem<ItemJobBinding>() {
     override fun getId() = job.uid.hashCode().toLong()
 
-    override fun bind(viewBinding: ItemJobBinding, position: Int) {
-        viewBinding.job = job
-        Picasso.get().load("https://picsum.photos/200").into(viewBinding.imageView)
-        viewBinding.imageView.setOnClickListener {
+    override fun bind(viewBinding: ItemJobBinding, position: Int) = with(viewBinding) {
+        job = this@JobItem.job
+        Picasso.get().load("https://picsum.photos/200").into(imageView)
+        imageView.setOnClickListener {
             val extras = FragmentNavigatorExtras(
-                viewBinding.imageView to "image",
-                viewBinding.title to "title",
-                viewBinding.description to "description"
+                imageView to "image",
+                title to "title",
+                description to "description"
             )
-            val action = JobsFragmentDirections.actionDestinationJobsToDestinationJobDetails(job)
+            val action =
+                JobsFragmentDirections.actionDestinationJobsToDestinationJobDetails(this@JobItem.job)
             findNavController(viewBinding.imageView)
-                .navigate(R.id.action_destination_jobs_to_destination_job_details, action.arguments, null, extras)
+                .navigate(
+                    R.id.action_destination_jobs_to_destination_job_details,
+                    action.arguments, null, extras
+                )
         }
 
         // highlight card if is selected
-        if (isSelected) viewBinding.cardView.setBackgroundColor(Color.CYAN)
-        else viewBinding.cardView.setBackgroundColor(Color.WHITE)
+        cardView.setBackgroundColor(if (isSelected) Color.CYAN else Color.WHITE)
     }
 
     override fun getLayout() = R.layout.item_job
