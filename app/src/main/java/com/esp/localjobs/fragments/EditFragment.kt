8afc6484831bcc8bd2300672912f4f8c3836bbd3
luca <lucamoro96@gmail.com>
2019-06-02 15:57:17 +0200

    Update item

--- app/src/main/java/com/esp/localjobs/fragments/EditFragment.kt
+++ app/src/main/java/com/esp/localjobs/fragments/EditFragment.kt
@@ -4,10 +4,14 @@ package com.esp.localjobs.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.forEach
 import androidx.navigation.fragment.navArgs
 import com.esp.localjobs.R
+import kotlinx.android.synthetic.main.fragment_edit.*
 
 class EditFragment : Fragment() {
     private val args: EditFragmentArgs by navArgs()
@@ -15,6 +19,7 @@ class EditFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_edit, container, false)
     }
 
@@ -24,8 +29,18 @@ class EditFragment : Fragment() {
 
     private fun setView() {
         val job = args.job
+        title_edit_text.setText(job.title)
+        location_edit_text.setText(job.city)
+        range_seekbar.progress = 2 // TODO once unified use job.range?
+        salary_edit_text.setText(job.salary)
+        description_edit_text.setText(job.description)
+    }
 
-        
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.forEach { it.isVisible = false }
+        inflater.inflate(R.menu.menu_save, menu)
+        val saveItem = menu.findItem(R.id.menu_save_item)
+
     }
 
 
