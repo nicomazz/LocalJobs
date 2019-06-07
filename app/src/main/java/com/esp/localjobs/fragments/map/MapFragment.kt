e6b9f5aa1a05f2b03cc75fee0f0636e5b5ee094c
Francesco Pham <pham.francesco@gmail.com>
2019-06-07 12:18:57 +0200

    ktlint fix

--- app/src/main/java/com/esp/localjobs/fragments/map/MapFragment.kt
+++ app/src/main/java/com/esp/localjobs/fragments/map/MapFragment.kt
@@ -1,7 +1,6 @@
 package com.esp.localjobs.fragments.map
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
