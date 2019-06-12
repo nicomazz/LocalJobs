f8708cfdfe5362977e27fe34118bf8bc2bfd834e
luca <lucamoro96@gmail.com>
2019-06-12 12:46:19 +0200

    storing last used filter params

--- app/src/main/java/com/esp/localjobs/viewModels/FilterViewModel.kt
+++ app/src/main/java/com/esp/localjobs/viewModels/FilterViewModel.kt
@@ -1,9 +1,11 @@
 package com.esp.localjobs.viewModels
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.esp.localjobs.LocalJobsApplication
+import com.esp.localjobs.R
 import com.esp.localjobs.data.models.Location
 import com.esp.localjobs.data.repository.JobsRepository.JobFilter
 import com.esp.localjobs.data.repository.MAX_RANGE_KM
@@ -36,21 +38,21 @@ class FilterViewModel : ViewModel() {
 
     init {
         val context = LocalJobsApplication.applicationContext()
-        PositionManager.getLastKnownPosition(context)?.let {
-            val city = GeocodingUtils.coordinatesToCity(context, it.latitude, it.longitude)
-            val location = Location(it.latitude, it.longitude, city)
+        val filter = retrieveLastUsedFilter(context)
 
-            setFilters(JobFilter(
-                location = location,
-                filteringJobs = true,
-                range = range,
-                salary = salary
-            ))
-        } ?: setFilters(JobFilter(filteringJobs = true, range = range, salary = salary))
+        if (filter.location == null) {
+            PositionManager.getLastKnownPosition(context)?.let {
+                val city = GeocodingUtils.coordinatesToCity(context, it.latitude, it.longitude)
+                filter.location = Location(it.latitude, it.longitude, city)
+            }
+        }
+
+        setFilters(filter)
     }
 
     fun setFilters(newFilters: JobFilter) {
         _activeFilters.postValue(newFilters)
+        updateLastUsedFilter(LocalJobsApplication.applicationContext(), newFilters)
     }
 
     fun setQuery(newQuery: String) {
@@ -67,5 +69,35 @@ class FilterViewModel : ViewModel() {
                 location = newLocation
             }
         )
+    }
+
+    private fun retrieveLastUsedFilter(context: Context): JobFilter {
+        val sharedPref = context.getSharedPreferences("filter", Context.MODE_PRIVATE)
+        with(sharedPref) {
+            return JobFilter().also {
+                it.filteringJobs = getBoolean("filteringJobs", true)
+                it.range = getInt("range", MAX_RANGE_KM)
+                val latitude = getString("latitude", null)?.toDoubleOrNull()
+                val longitude = getString("longitude", null)?.toDoubleOrNull()
+                val city = getString("city", context.getString(R.string.unknown_location))
+                if (latitude != null && longitude != null)
+                    it.location = Location(latitude, longitude, city)
+            }
+        }
+    }
+
+    private fun updateLastUsedFilter(context: Context, newFilter: JobFilter) {
+        val sharedPreferences = context.getSharedPreferences("filter", Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putBoolean("filteringJobs", newFilter.filteringJobs)
+            putInt("range", newFilter.range)
+            newFilter.location?.let {
+                val (lat, lng) = it.latLng()
+                putString("latitude", lat.toString())
+                putString("longitude", lng.toString())
+                putString("city", it.city)
+            }
+            commit()
+        }
     }
 }
