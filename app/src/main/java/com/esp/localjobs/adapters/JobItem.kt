84a437b3572afd64b01b5b4a5597a3ee4736cd58
Luca Moroldo <44212562+lucamoroz@users.noreply.github.com>
2019-06-16 19:42:26 +0200

    refactored job layout (#79)
    * new job layout draft
    
    * add author bind in JobItem
    
    * ktlint fix

--- app/src/main/java/com/esp/localjobs/adapters/JobItem.kt
+++ app/src/main/java/com/esp/localjobs/adapters/JobItem.kt
@@ -7,27 +7,44 @@ import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.esp.localjobs.R
 import com.esp.localjobs.data.models.Job
+import com.esp.localjobs.data.models.User
+import com.esp.localjobs.data.repository.userFirebaseRepository
 import com.esp.localjobs.databinding.ItemJobBinding
 import com.esp.localjobs.fragments.JobsFragmentDirections
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.databinding.BindableItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
+@InternalCoroutinesApi
 class JobItem(val job: Job) : BindableItem<ItemJobBinding>() {
 
     override fun getId() = job.uid.hashCode().toLong()
 
     override fun bind(viewBinding: ItemJobBinding, position: Int) = with(viewBinding) {
         job = this@JobItem.job
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val author = this@JobItem.job.uid?.let {
+                userFirebaseRepository.getUserDetails(it)
+            } ?: User(displayName = "Unknown user")
+            setAuthor(author)
+        }
+
         Picasso.get().load("https://picsum.photos/400").into(imageView)
+
         cardView.clipToOutline = false // without this, shared elements are cropped
         imageView.transitionName = "image_${this@JobItem.job.uid}"
         title.transitionName = "title_${this@JobItem.job.uid}"
-        description.transitionName = "description_${this@JobItem.job.uid}"
+        // description.transitionName = "description_${this@JobItem.job.uid}"
+
         cardView.setOnClickListener {
             val extras = FragmentNavigatorExtras(
                 imageView to "image_${this@JobItem.job.uid}",
-                title to "title_${this@JobItem.job.uid}",
-                description to "description_${this@JobItem.job.uid}"
+                title to "title_${this@JobItem.job.uid}"
+                // description to "description_${this@JobItem.job.uid}"
             )
             val action =
                 JobsFragmentDirections.actionDestinationJobsToDestinationJobDetails(this@JobItem.job)
