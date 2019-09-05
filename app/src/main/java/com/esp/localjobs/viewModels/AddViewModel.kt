e11f6ff7af6ec905c26659ac0a55a69ff187d61d
Luca Moroldo <44212562+lucamoroz@users.noreply.github.com>
2019-09-05 11:02:53 +0200

    Feature/job add edit refactor (#112)
    * add job argument for addJobFragment
    
    * new android studio file?
    
    * updated gradle ver
    
    * add & edit on the same fragment, allowing image edit
    
    * deleted old edit fragment
    
    * ktlint
    
    * removed todo comment
    
    * better syntax

--- app/src/main/java/com/esp/localjobs/viewModels/AddViewModel.kt
+++ app/src/main/java/com/esp/localjobs/viewModels/AddViewModel.kt
@@ -7,6 +7,8 @@ import com.esp.localjobs.data.models.Job
 import com.esp.localjobs.data.repository.JobsRepository
 
 class AddViewModel : ViewModel() {
+    // todo use interface
+
     fun addJobToRepository(
         job: Job,
         onSuccess: (() -> Unit),
@@ -24,5 +26,38 @@ class AddViewModel : ViewModel() {
                 }
             }
         )
+    }
+
+    fun update(
+        id: String,
+        newJob: Job,
+        onSuccess: (() -> Unit),
+        onFailure: ((e: Exception) -> Unit)
+    ) {
+        JobsRepository().update(id, newItem = newJob, callback = object : BaseRepository.EventCallback {
+            override fun onSuccess() {
+                onSuccess()
+            }
+
+            override fun onFailure(e: Exception) {
+                onFailure(e)
+            }
+        })
+    }
+
+    fun delete(
+        id: String,
+        onSuccess: (() -> Unit),
+        onFailure: ((e: Exception) -> Unit)
+    ) {
+        JobsRepository().delete(id, callback = object : BaseRepository.EventCallback {
+            override fun onSuccess() {
+                onSuccess()
+            }
+
+            override fun onFailure(e: Exception) {
+                onFailure(e)
+            }
+        })
     }
 }
